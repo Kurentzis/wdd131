706fa8c90d1fce16813fcefe676305ff50d101e6
Oleg Lasukov <lasukov2016@mail.ru>
2025-05-29 18:41:28 +0500

    feat: ability to select aquarium on felt calc and monitor

--- project/scripts/monitor.js
+++ project/scripts/monitor.js
@@ -6,7 +6,6 @@ let dataArrayBeforeUDO = [];
 let dataArrayDose = [];
 let dataLabel = [];
 
-
 function loadHistory() {
   const history = JSON.parse(localStorage.getItem("fertilizerHistory")) || [];
   const historyContainer = document.getElementById("historyItems");
@@ -15,72 +14,82 @@ function loadHistory() {
     historyContainer.innerHTML = "<p>No measurements recorded yet.</p>";
     return;
   }
-  let element = document.getElementById('element').value
+  let element = document.getElementById("element").value;
+  let aquarium = document.getElementById("aquarium").value;
 
   history.forEach((item) => {
     try {
-      if(item.element.toLowerCase() == element) {
-      const historyItem = document.createElement("div");
-      let label = item.timestamp;
-      labels.push(label);
-      // labels.sort();
+      if (
+        item.element.toLowerCase() == element &&
+        item.aquariumName.toLowerCase() == aquarium
+      ) {
+        const historyItem = document.createElement("div");
+        let label = item.timestamp;
+        labels.push(label);
+        // labels.sort();
 
-      dataArrayTotalConcentration.push(item.concentration);
-      dataArrayBeforeUDO.push(item.currentConc);
-      dataArrayDose.push(item.dose);
-    
-      let dataObj = {
-        data: dataArrayTotalConcentration,
-        label: "Концентрация после УДО"
+        dataArrayTotalConcentration.push(item.concentration);
+        dataArrayBeforeUDO.push(item.currentConc);
+        dataArrayDose.push(item.dose);
+
+        let dataObj = {
+          data: dataArrayTotalConcentration,
+          label: "Концентрация после УДО",
+        };
       }
-   }
     } catch (error) {
-      console.log(item.id)
+      console.log(item.id);
     }
-
-
   });
   var myChart = new Chart(ctx, {
-  type: "line",
-  data: {
-    labels: labels,
-    datasets: [
-      {
-        data: dataArrayDose,//[10, 21, 60, 44, 17, 21, 17],
-        label: "Внесенная доза",
-        borderColor: "#ffa500",
-        backgroundColor: "#ffc04d",
-        fill: false,
-      },
-      {
-        data: dataArrayTotalConcentration,
-        label: "Концентрация после УДО",
-        borderColor: "#3e95cd",
-        backgroundColor: "#7bb6dd",
-        fill: false,
-      },
-      {
-        data: dataArrayBeforeUDO,//[70, 90, 44, 60, 83, 90, 100],
-        label: "Концентрация до УДО",
-        borderColor: "#3cba9f",
-        backgroundColor: "#71d1bd",
-        fill: false,
-      },
-
-    ],
-  },
-});
-
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          data: dataArrayDose, //[10, 21, 60, 44, 17, 21, 17],
+          label: "Внесенная доза",
+          borderColor: "#ffa500",
+          backgroundColor: "#ffc04d",
+          fill: false,
+        },
+        {
+          data: dataArrayTotalConcentration,
+          label: "Концентрация после УДО",
+          borderColor: "#3e95cd",
+          backgroundColor: "#7bb6dd",
+          fill: false,
+        },
+        {
+          data: dataArrayBeforeUDO, //[70, 90, 44, 60, 83, 90, 100],
+          label: "Концентрация до УДО",
+          borderColor: "#3cba9f",
+          backgroundColor: "#71d1bd",
+          fill: false,
+        },
+      ],
+    },
+  });
 }
 
-document.getElementById('element').addEventListener("change", function() {
+document.getElementById("element").addEventListener("change", function () {
   labels = [];
   dataSets = [];
   dataArrayTotalConcentration = [];
   dataArrayBeforeUDO = [];
   dataArrayDose = [];
   dataLabel = [];
-  loadHistory()
+  loadHistory();
+});
+
+document.getElementById("aquarium").addEventListener("change", function () {
+  labels = [];
+  dataSets = [];
+  dataArrayTotalConcentration = [];
+  dataArrayBeforeUDO = [];
+  dataArrayDose = [];
+  dataLabel = [];
+  loadHistory();
 });
 
 document.addEventListener("DOMContentLoaded", function () {
